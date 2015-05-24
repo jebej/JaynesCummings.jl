8915d0b9315aca8c7972ca01c08d1599999bde0e
JeremyBejanin <jbejanin@uwaterloo.ca>
2015-05-23 23:02:15 -0400

    Removed some unnecessary array allocation

--- src/calc_qubittimeevo.jl
+++ src/calc_qubittimeevo.jl
@@ -2,10 +2,9 @@ function calc_qubittimeevo(initialstate,time_evo_array)
   excited_prob = zeros(Complex128,length(time_evo_array))
 
   # Time evolve the system density matrix and "measure" the qubit at each time sample taken. This is done by
-  # tracing out the resonator and keeping the excited state entry of the density matrix
+  # tracing out the resonator and keeping the excited state entry of the density matrix (ie, entry 2,2 in the matrix)
   for i=1:length(time_evo_array)
-    temp= partialtrace(time_evo_array[i] * initialstate * time_evo_array[i]',2) # trace out the resonator system from the density matrix
-    excited_prob[i] = temp[2,2] # only keep the excited state probability (ie, entry 2,2 in the matrix)
+    excited_prob[i] = partialtrace(time_evo_array[i] * initialstate * time_evo_array[i]',2)[2,2] 
   end
 
   return excited_prob
--- src/gen_timeevoarray.jl
+++ src/gen_timeevoarray.jl
@@ -3,9 +3,8 @@ function gen_timeevoarray(hamiltonian,finalTime,samples)
   time_vec=((1:samples)-1)*finalTime/samples
   time_evo_array = Array(typeof(hamiltonian),samples)
 
-  for i=1:samples
-    timeevomat = expm(-1im .* hamiltonian .* time_vec[i] ./ h_bar)
-    time_evo_array[i] = timeevomat
+  for i=1:samples 
+    time_evo_array[i] = expm(-1im .* hamiltonian .* time_vec[i] ./ h_bar)
   end
 
   return time_vec, time_evo_array
