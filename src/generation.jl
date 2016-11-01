30cf73bd90ff3c73dc8e1b9a75723c768bc202c4
JeremyBejanin <jbejanin@uwaterloo.ca>
2016-11-01 15:11:29 -0400

    Cleaner notation for time evolution function.

--- src/generation.jl
+++ src/generation.jl
@@ -90,15 +90,12 @@ function gen_hamiltonian(ω_q,ω_r,g,cutoffN,noiseAdded)
 end
 
 
-function gen_timeevoarray(hamiltonian,finalTime,samples)
+function gen_timeevoarray(H,finalTime,samples)
     # Generate the time evolution array for a time independent Hamiltonian
-    time_vec=((1:samples)-1)*finalTime/samples
-    time_evo_array = Array(typeof(hamiltonian),samples)
-
-    for i=1:samples
-        time_evo_array[i] = expm(-1.0im .* hamiltonian .* time_vec[i] / ħ)
+    time_vec=(0:samples-1)*finalTime/samples
+    time_evo_array = map(time_vec) do t
+        expm(-1.0im * H * t / ħ)
     end
-
     return time_vec, time_evo_array
 end
 
